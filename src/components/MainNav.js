50677e4945d29e7f9f76bc2bee633337c6aae5d9
pavan.gupta@algorisys.com <pavan.gupta@algorisys.com>
2022-04-17 00:40:13 +0530

    updated navigation footer

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -9,61 +9,67 @@ import WhatshotIcon from "@material-ui/icons/Whatshot";
 import { useNavigate } from "react-router-dom";
 
 const useStyles = makeStyles({
-  root: {
-    width: "100%",
-    position: "fixed",
-    bottom: 0,
-    backgroundColor: "#2d313a",
-    zIndex: 100,
-  },
+    root: {
+        width: "100%",
+        position: "fixed",
+        bottom: 0,
+        backgroundColor: "#2d313a",
+        zIndex: 100,
+    },
 });
 
 export default function SimpleBottomNavigation() {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
-//   const navigate = useNavigate();
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+    const navigate = useNavigate();
 
-//   useEffect(() => {
-//     if (value === 0) {
-//       navigate("/");
-//     } else if (value === 1) {
-//       navigate("/movies");
-//     } else if (value === 2) {
-//       navigate("/series");
-//     } else if (value === 3) {
-//       navigate("/search");
-//     }
-//   }, [value, navigate]);
+    useEffect(() => {
 
-  return (
-    <BottomNavigation
-      value={value}
-      onChange={(event, newValue) => {
-        setValue(newValue);
-      }}
-      showLabels
-      className={classes.root}
-    >
-      <BottomNavigationAction
-        style={{ color: "white" }}
-        label="Trending"
-        icon={<WhatshotIcon />}
-      />
-      <BottomNavigationAction
-        style={{ color: "white" }}
-        label="Movies"
-        icon={<MovieIcon />}
-      />
-      <BottomNavigationAction
-        style={{ color: "white" }}
-        label="TV Series"
-        icon={<TvIcon />}
-      />
-      <BottomNavigationAction
-        style={{ color: "white" }}
-        label="Search"
-        icon={<SearchIcon />}
-      />
-    </BottomNavigation>
-  );
+        switch (value) {
+            case 0:
+                navigate("/");
+                break;
+            case 1:
+                navigate("/movies");
+                break;
+            case 2:
+                navigate("/series");
+                break;
+            case 3:
+                navigate("/search");
+                break;
+        }
+    }, [value, navigate]);
+
+    return (
+        <BottomNavigation
+            value={value}
+            onChange={(event, newValue) => {
+                setValue(newValue);
+            }}
+            showLabels
+            className={classes.root}
+        >
+            <BottomNavigationAction
+                style={{ color: "white" }}
+                label="Trending"
+                icon={<WhatshotIcon />}
+            />
+            <BottomNavigationAction
+                style={{ color: "white" }}
+                label="Movies"
+                icon={<MovieIcon />}
+            />
+            <BottomNavigationAction
+                style={{ color: "white" }}
+                label="TV Series"
+                icon={<TvIcon />}
+            />
+            <BottomNavigationAction
+                style={{ color: "white" }}
+                label="Search"
+                icon={<SearchIcon />}
+            />
+        </BottomNavigation>
+    );
 }
