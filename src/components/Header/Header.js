78dcf51a8dc87d4aa680cb41a01807c2dea1b0ea
pavan.gupta@algorisys.com <pavan.gupta@algorisys.com>
2022-04-17 00:24:42 +0530

    setup routes and header,footer

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+const Header = () => {
+    return (
+        <span className='header'>
+             &#128253; Entertainment Hub &#127871;
+        </span>
+    )
+}
+
+export default Header
